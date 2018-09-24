07fb373cc1183e501a777f74e2f6c0e65dd292ca
koyanagibiz <koyanagibiz@gmail.com>
2018-09-24 12:55:54 +0900

    課題提出
    tagを使用し、groundとcubeに衝突する場合のみaudiosourceを再生するように設定

---
+++ Assets/CubeSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CubeSound : MonoBehaviour {
+
+	//宣言
+	private AudioSource hitsound;
+
+	void Start () {
+
+		//コンポーネント取得
+		hitsound = GetComponent<AudioSource>();
+
+
+	}
+
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		//判定確認用ログ
+		Debug.Log ("OnCollisionEnter2D: " + other.gameObject.tag);
+	
+		//groundとcubeに衝突した時のみ音を鳴らす
+		if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Cube") {
+			hitsound.Play();
+		}
+	}
+}
